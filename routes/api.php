2f75c91520b134818dc1ad1d9c56a554e8178c9b
RamananaThuMingThierry <ramananathumingthierry@gmail.com>
2022-12-22 12:53:16 +0300

    Private route in react Js with Authentification for Admin - axios Interceptor

--- routes/api.php
+++ routes/api.php
@@ -9,9 +9,12 @@ Route::post('register', [AuthController::class, 'register']);
 
 Route::middleware(['auth:sanctum'])->group(function(){
 
+    Route::get('/checkingAuthenticated', function(){
+        return response()->json([
+             'message' => 'You are in',
+            'status' => 200], 200);
+    });
+    
     Route::post('logout', [AuthController::class, 'logout']);
 });
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
