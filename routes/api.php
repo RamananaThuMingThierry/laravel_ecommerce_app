b94cdc8aaf2738186801e21fe9b54254cea0f74e
RamananaThuMingThierry <ramananathumingthierry@gmail.com>
2022-12-22 09:14:04 +0300

    Mettre en place le button Déconnexion

--- routes/api.php
+++ routes/api.php
@@ -7,6 +7,11 @@ use App\Http\Controllers\API\AuthController;
 Route::post('login', [AuthController::class, 'login']);
 Route::post('register', [AuthController::class, 'register']);
 
+Route::middleware(['auth:sanctum'])->group(function(){
+
+    Route::post('logout', [AuthController::class, 'logout']);
+});
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
